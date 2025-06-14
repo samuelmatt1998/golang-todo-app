b88397249517a66e4f54455c186e824399079b32
Kevin <kevin@differentbyte.in>
2025-06-14 12:17:25 +0530

    added git ignore

--- models/models.go
+++ models/models.go
@@ -1,3 +1,16 @@
 package models
 
-import "github.com/kevin/todo-app/config"
+import (
+	"github.com/samuelmatt1998/golang-todo-app/config"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	
+)
+
+var TaskCollection *mongo.Collection = config.DB.Collection("tasks")
+
+type Task struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Task       string             `bson:"task" json:"task"`
+	TaskStatus string             `bson:"task_status" json:"task_status"` // "done" or "not done"
+}
